5377f56048f1e0350ff27c009aeef32841c394b4
moritzjenny <moritzjenny@hotmail.com>
2021-11-12 23:19:40 +0100

    add logic

--- api/api.py
+++ api/api.py
@@ -1,11 +1,90 @@
 from flask import Flask
+import asyncio
+import json
+import mysql_getter
+import sys
+from flask_socketio import SocketIO
+sys.path.append('/home/moritzjenny/chickenBot')
+import servo_controller
+import grove_button
+import camera_controller
+
+async def loop():
+    while True:
+        name = camera_controller.take_still()
+        updateImage(name)
+        await asyncio.sleep(60)
+
+
 
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
+# Kickoff timer
+#asyncio.run(loop())
+# name = camera_controller.take_still()
+# updateImage(name)
+
+
+# Button
+pin = 16
+button = grove_button.GroveButton(pin)
+
+def on_press(t):
+	print('Button is pressed')
+	servo_controller.turn()
+
+def on_release(t):
+	print("Button is released, pressed for {0} seconds".format(round(t, 6)))
+
+button.on_press = on_press
+button.on_release = on_release
+
+socketio.run(app)
+
+if __name__ == '__main__':
+	socketio.run(app)
+
+
+
+
+def get_time_table():
+    with open("/home/moritzjenny/chickenBot/data/time_settings/timetable.json") as jsonFile:
+        t = json.load(jsonFile)
+        jsonFile.close()
+        morning = t["morning"]
+        evening = t["evening"]
+        return morning, evening
+
+def write_time_table_morning(newMorning):
+    morning, evening = get_time_table()
+    json_data = {
+		"morning": newMorning,
+		"evening": evening
+	}
+
+    with open('/home/moritzjenny/chickenBot/data/time_settings/timetable.json', 'w') as jsonFile:
+        json.dump(json_data, jsonFile)
+        jsonFile.close()
+
+def write_time_table_evening(newEvening):
+	morning, evening = get_time_table()
+
+	json_data = {
+		"morning": morning,
+		"evening": newEvening
+	}
+
+	with open('/home/moritzjenny/chickenBot/data/time_settings/timetable.json', 'w') as jsonFile:
+		json.dump(json_data, jsonFile)
+		jsonFile.close()
 
 @app.route('/data')
-def get_temp_hum_data():
+def get_data():
+
+	# get from file
+	"""
 	f = open("/home/moritzjenny/chickenBot/data/temp_hum/temp.txt", "r")
 	temp = f.readlines()[-1]
 	f.close()
@@ -13,3 +92,44 @@ def get_temp_hum_data():
 	hum = f.readlines()[-1]
 	f.close()
 	return {'temp': temp, 'humi': hum}
+	"""
+
+	# get from db
+	temp = mysql_getter.get_newest_temp()
+	hum = mysql_getter.get_newest_humi()
+
+	# get timetable
+	morning, evening = get_time_table()
+
+	return {'temp': temp, 'humi': hum, 'morning': morning, 'evening': evening}
+
+
+@app.route('/triggerDoor')
+def triggerDoorFromClient():
+	servo_controller.turn()
+	return {}
+
+
+@socketio.on('connect')
+def connected():
+	print("Connected to client")
+
+@socketio.on('disconnect')
+def disconnected():
+	print('Disconnected from client')
+
+def updateImage(name):
+	print('send new image name ...  ' + name)
+	socketio.emit('updateImage', {'data': name})
+
+def triggerDoorFromBackend():
+	print('triggering door from client ... ')
+	socketio.emit('triggerDoorResponse')
+
+
+def finishedTurn(message):
+	print('finished turning door from client ... ')
+	socketio.emit('finishedDoorResponse', {'data': message})
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,49 +1,129 @@
 import React, {useState, useEffect} from 'react';
+import { Button } from 'react-bootstrap-buttons';
+import 'react-bootstrap-buttons/dist/react-bootstrap-buttons.css';
+import {ReactSpinner} from 'react-spinning-wheel';
+import 'react-spinning-wheel/dist/style.css';
+import TimePicker from 'react-time-picker';
 import './App.css';
 import TemperatureWarmIcon from './temperature-warm.jsx';
 import HumidityIcon from './humidity.jsx';
+import {io} from 'socket.io-client';
+
+const socket = io();
+
 
 function App() {
 	const [temp, setTemp] = useState(0);
     const [humi, setHumi] = useState(0);
+    const [morningValue, onChangeMorningValue] = useState('10:00');
+    const [eveningValue, onChangeEveningValue] = useState('19:00');
+
+    const [doorState, setDoorState] = useState("opened");
+    const [imgName, setImgName] = useState("../../chickenBot/data/images/watermarked_Nov-12-2021_22:35:56.jpg");
 
 	useEffect(() => {
 	    fetch('/data').then(res => res.json()).then(data => {
             setTemp(data.temp);
             setHumi(data.humi);
+            onChangeMorningValue(data.morning);
+            onChangeEveningValue(data.evening);
         });
     }, []);
+
+    const triggerDoor=()=>{
+        fetch("/triggerDoor")
+    };
+
+    useEffect(() => {
+        socket.on('triggerDoorResponse', (resp) => {
+            setDoorState("moving");
+            console.log("triggerDoorResponse");
+        });
+    }, []);
+
+    useEffect(() => {
+        socket.on('finishedDoorResponse', (resp) => {
+            setDoorState(resp.data);
+            console.log("finishedDoorResponse");
+        });
+    }, []);
+
+    useEffect(() => {
+        socket.on('updateImage', (resp) => {
+            alert(resp.data);
+            console.log("updateImage");
+        });
+    }, []);
+
+
 
     return (
     <div className="content">
         <header className="App-header">
             <span className="title">Chickenbot.ch</span>
             <div className="rooster-wrapper">
-                <img src="rooster.png" className="rooster-image"></img>
+                <img src="rooster.png" className="rooster-image" alt="rooster"></img>
             </div>
         </header>
-        <div class="grid">
-            <div class="rcorners rcorner-0 tile-wrapper">
-                <div class="tile-row">
+        <div className="grid">
+            <div className="rcorners rcorner-0 tile-wrapper">
+                <div className="tile-row">
                     <TemperatureWarmIcon className="icon"/>
                     <span>{temp}°C</span>
                 </div>
-                <div class="tile-row">
+                <div className="tile-row">
                     <HumidityIcon className="icon" fill="#2980B9"/>
                     <span>{humi}%</span>
                 </div>
             </div>
-            <div class="rcorners tile-text-row">
+            <div className="rcorners tile-text-row">
                 <span>Zuletzt geöffnet</span>
-                <span>10.11.2021 08:43</span>
+                <span>10.11.2021 08:23</span>
             </div>
-            <div class="rcorners tile-keyword-row">OFFEN</div>
+            <div className="rcorners tile-keyword-row">
+                <span>OFFEN</span>
+            </div>
             <div className="rcorners rcorner-3">
                 <div className="tile-title">Kamera</div>
-                <img src="view.jpg" className="webcam"></img>
+                <img src={imgName} className="webcam" alt=""></img>
             </div>
             <div className="rcorners">
                 <div className="tile-title">Steuerung</div>
+                <div className="control-wrapper">
+
+                    <span>Öffnung um</span>
+                    <TimePicker
+                        onChange={onChangeMorningValue}
+                        value={morningValue}
+                    />
+                    <div></div>
+                    <span>Schliessung um</span>
+                    <TimePicker
+                        onChange={onChangeEveningValue}
+                        value={eveningValue}
+                    />
+                    <div></div>
+                    <div></div>
+
+                    {
+                        doorState==="closed"&&(
+                        <Button lg btnStyle="default" onClick={triggerDoor}>Öffnen</Button>
+                        )
+                    }
+                    {
+                        doorState==="opened"&&(
+                            <Button lg btnStyle="default" onClick={triggerDoor}>Schliessen</Button>
+                        )
+                    }
+                    {
+                        (doorState==="moving")&&(
+                            <div>
+                                <ReactSpinner />
+                            </div>
+                        )
+                    }
+                </div>
+
             </div>
         </div>
     </div>
